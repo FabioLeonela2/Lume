1eac2479903f90fe198db5007b9246f02a178b89
FabioLeonela2 <fabioleonelvalenzohilerio@gmail.com>
2025-07-10 14:48:59 -0500

    Add files via upload

---
+++ Lumecasiterminado.sql
@@ -0,0 +1,182 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: lumeapp
+-- ------------------------------------------------------
+-- Server version	9.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `emociones`
+--
+
+DROP TABLE IF EXISTS `emociones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `emociones` (
+  `id_emocion` int NOT NULL AUTO_INCREMENT,
+  `emocion` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_usuario` int NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_emocion`),
+  KEY `emociones_ibfk_1` (`id_usuario`),
+  CONSTRAINT `emociones_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `emociones`
+--
+
+LOCK TABLES `emociones` WRITE;
+/*!40000 ALTER TABLE `emociones` DISABLE KEYS */;
+INSERT INTO `emociones` VALUES (1,'eu',1,'2025-07-09 14:56:59'),(2,'Feliz',1,'2025-07-09 14:57:30'),(3,'Feliz',1,'2025-07-09 14:57:41');
+/*!40000 ALTER TABLE `emociones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `intensidad_emocional`
+--
+
+DROP TABLE IF EXISTS `intensidad_emocional`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `intensidad_emocional` (
+  `id_intensidad` int NOT NULL AUTO_INCREMENT,
+  `porcentaje` int NOT NULL,
+  `id_emocion` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `id_nota` int NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_intensidad`),
+  KEY `intensidad_emocional_ibfk_1` (`id_emocion`),
+  KEY `intensidad_emocional_ibfk_2` (`id_usuario`),
+  KEY `intensidad_emocional_ibfk_3` (`id_nota`),
+  CONSTRAINT `intensidad_emocional_ibfk_1` FOREIGN KEY (`id_emocion`) REFERENCES `emociones` (`id_emocion`),
+  CONSTRAINT `intensidad_emocional_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  CONSTRAINT `intensidad_emocional_ibfk_3` FOREIGN KEY (`id_nota`) REFERENCES `notausuario` (`id_nota`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `intensidad_emocional`
+--
+
+LOCK TABLES `intensidad_emocional` WRITE;
+/*!40000 ALTER TABLE `intensidad_emocional` DISABLE KEYS */;
+INSERT INTO `intensidad_emocional` VALUES (2,80,2,1,2,'2025-07-10 14:58:47');
+/*!40000 ALTER TABLE `intensidad_emocional` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `meta`
+--
+
+DROP TABLE IF EXISTS `meta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `meta` (
+  `id_meta` int NOT NULL AUTO_INCREMENT,
+  `descripcion` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_usuario` int NOT NULL,
+  `id_emocion` int NOT NULL,
+  `id_nota` int NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_meta`),
+  KEY `meta_ibfk_1` (`id_usuario`),
+  KEY `meta_ibfk_2` (`id_emocion`),
+  KEY `meta_ibfk_3` (`id_nota`),
+  CONSTRAINT `meta_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  CONSTRAINT `meta_ibfk_2` FOREIGN KEY (`id_emocion`) REFERENCES `emociones` (`id_emocion`),
+  CONSTRAINT `meta_ibfk_3` FOREIGN KEY (`id_nota`) REFERENCES `notausuario` (`id_nota`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `meta`
+--
+
+LOCK TABLES `meta` WRITE;
+/*!40000 ALTER TABLE `meta` DISABLE KEYS */;
+INSERT INTO `meta` VALUES (1,'sal a correr',1,2,2,'2025-07-09 15:57:08');
+/*!40000 ALTER TABLE `meta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notausuario`
+--
+
+DROP TABLE IF EXISTS `notausuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notausuario` (
+  `id_nota` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contenido` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_usuario` int NOT NULL,
+  `id_emocion` int NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_nota`),
+  KEY `notausuario_ibfk_1` (`id_usuario`),
+  KEY `notausuario_ibfk_2` (`id_emocion`),
+  CONSTRAINT `notausuario_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  CONSTRAINT `notausuario_ibfk_2` FOREIGN KEY (`id_emocion`) REFERENCES `emociones` (`id_emocion`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notausuario`
+--
+
+LOCK TABLES `notausuario` WRITE;
+/*!40000 ALTER TABLE `notausuario` DISABLE KEYS */;
+INSERT INTO `notausuario` VALUES (1,'prueba','pru',1,2,'2025-07-09 15:42:06'),(2,'hggf','fgf',1,2,'2025-07-09 15:43:54');
+/*!40000 ALTER TABLE `notausuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Correo` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Contraseña` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Leo','Leo@gmail.com','123456','2025-07-09 13:37:07');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-07-10 14:46:34
